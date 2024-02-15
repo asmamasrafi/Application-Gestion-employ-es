05e06225684059be1535eed91175194c12bc4f35
asmamasrafi <106540206+asmamasrafi@users.noreply.github.com>
2024-02-15 15:04:11 +0100

    Add files via upload

---
+++ PAROJET_JAVA/src/application/FXMLDocumentController.java
@@ -0,0 +1,105 @@
+package application;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+public class FXMLDocumentController {
+
+    @FXML
+    private Button loginBtn;
+
+    @FXML
+    private PasswordField password;
+
+    @FXML
+    private TextField username;
+    
+//  DATABASE TOOLS
+  private Connection connect;
+  private PreparedStatement prepare;
+  private ResultSet result;
+
+  private double x = 0;
+  private double y = 0;
+
+    @FXML
+    void loginAdmin() {
+    	  
+        String sql = "SELECT * FROM admin WHERE username = ? and password = ?";
+        
+        connect = database.connectDb();
+        
+        try{
+            prepare = connect.prepareStatement(sql);
+            prepare.setString(1, username.getText());
+            prepare.setString(2, password.getText());
+            
+            result = prepare.executeQuery();
+            Alert alert;
+            
+            if(username.getText().isEmpty() || password.getText().isEmpty()){
+                alert = new Alert(AlertType.ERROR);
+                alert.setTitle("Error Message");
+                alert.setHeaderText(null);
+                alert.setContentText("Please fill all blank fields");
+                alert.showAndWait();
+            }else{
+                if(result.next()){
+                    getData.username = username.getText();
+                    
+                    alert = new Alert(AlertType.INFORMATION);
+                    alert.setTitle("Information Message");
+                    alert.setHeaderText(null);
+                    alert.setContentText("Successfully Login");
+                    alert.showAndWait();
+                    
+                    loginBtn.getScene().getWindow().hide();
+                    Parent root = FXMLLoader.load(getClass().getResource("dashboard.fxml"));
+                    Stage stage = new Stage();
+                    Scene scene = new Scene(root);
+                    
+                    root.setOnMousePressed((MouseEvent event) ->{
+                        x = event.getSceneX();
+                        y = event.getSceneY();
+                    });
+                    
+                    root.setOnMouseDragged((MouseEvent event) ->{
+                        stage.setX(event.getScreenX() - x);
+                        stage.setY(event.getScreenY() - y);
+                    });
+                    
+                    stage.initStyle(StageStyle.TRANSPARENT);
+                    stage.setScene(scene);
+                    stage.show();
+                    
+                }else{
+                    alert = new Alert(AlertType.ERROR);
+                    alert.setTitle("Error Message");
+                    alert.setHeaderText(null);
+                    alert.setContentText("Wrong Username/Password");
+                    alert.showAndWait();
+                }
+            }
+            
+        }catch(Exception e){
+        	e.printStackTrace();
+        }
+        
+
+    }
+
+}
